cc1a86d4a0d00f2fbc0c272bb71fd73eb5b52d1b
marcellasantoscruz <marcellacruz015@gmail.com>
2022-08-23 22:20:43 -0300

    2 files modified

--- index.js
+++ index.js
@@ -4,3 +4,9 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>My App</h1>`;
+
+//Functional programming example
+function getNumber(INPUTid){
+    const value = document.getElementById(inputID).value;
+    return pasrInt(value); 
+}
